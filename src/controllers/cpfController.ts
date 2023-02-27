365a3dc2d3e94e74c77daaa2ea0950a122be51ae
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-02-27 20:29:38 -0300

    feat: added all controllers

--- src/controllers/cpfController.ts
+++ src/controllers/cpfController.ts
@@ -1,11 +1,46 @@
 import { Request, Response } from "express";
+import {
+  createRestrictedCpf as createRestrictedCpfService,
+  findAllRestrictedCpf,
+  listRestrictedCpf,
+  deleteRestrictedCpf as deleteRestrictedCpfService,
+} from "../services/cpfServices.js";
+import { RestrictedCpf } from "../types/cpfTypes.js";
 
 export async function createRestrictedCpf(req: Request, res: Response) {
+  const cpf = req.body as RestrictedCpf;
+
+  await createRestrictedCpfService(cpf);
+
   return res.sendStatus(201);
 }
 
-export async function getRestrictedCpf(req: Request, res: Response) {}
+export async function getRestrictedCpf(req: Request, res: Response) {
+  const { cpf } = req.params;
 
-export async function deleteRestrictedCpf(req: Request, res: Response) {}
+  const cpfAsRestrictedCpf: RestrictedCpf = {
+    cpf,
+  };
 
-export async function getAllRestrictedCpf(req: Request, res: Response) {}
+  const restrictedCpf = await listRestrictedCpf(cpfAsRestrictedCpf);
+
+  return res.status(200).send(restrictedCpf);
+}
+
+export async function deleteRestrictedCpf(req: Request, res: Response) {
+  const { cpf } = req.params;
+
+  const cpfAsRestrictedCpf: RestrictedCpf = {
+    cpf,
+  };
+
+  await deleteRestrictedCpfService(cpfAsRestrictedCpf);
+
+  return res.sendStatus(200);
+}
+
+export async function getAllRestrictedCpf(req: Request, res: Response) {
+  const allRestrictedsCpfs = await findAllRestrictedCpf();
+
+  return res.status(200).send(allRestrictedsCpfs);
+}
