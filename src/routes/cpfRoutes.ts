841e544f4b409aece8d3c7f405ed73c09c4ec859
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-02-27 20:28:34 -0300

    feat: added all validations and routes

--- src/routes/cpfRoutes.ts
+++ src/routes/cpfRoutes.ts
@@ -4,12 +4,12 @@ import {
   createRestrictedCpf,
   deleteRestrictedCpf,
   getRestrictedCpf,
-} from "../controllers/cpfController";
-import { isValidCpfMiddleware } from "../middlewares/isValidCpfMiddleware";
+} from "../controllers/cpfController.js";
+import { isValidCpfMiddleware } from "../middlewares/isValidCpfMiddleware.js";
 
 export const router = express.Router();
 
-router.get("/cpf/{cpf}", getRestrictedCpf);
 router.get("/cpf", getAllRestrictedCpf);
+router.get("/cpf/:cpf", isValidCpfMiddleware, getRestrictedCpf);
 router.post("/cpf", isValidCpfMiddleware, createRestrictedCpf);
-router.delete("/cpf/{cpf}", deleteRestrictedCpf);
+router.delete("/cpf/:cpf", isValidCpfMiddleware, deleteRestrictedCpf);
