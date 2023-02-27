d7c4744e1eaaaa0f0f848cb38b0fdf81ad9dc23d
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-02-27 20:26:29 -0300

    fix: added .js to all imports

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
-import { errorHandlerMiddleware } from "./middlewares/errorHandler";
-import { router } from "./routes/cpfRoutes";
+import { errorHandlerMiddleware } from "./middlewares/errorHandler.js";
+import { router } from "./routes/cpfRoutes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
